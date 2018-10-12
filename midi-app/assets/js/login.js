49d9eb1e1a525b4a713eccac4d52e4a3d055b90f
Angel H <angel@adboomgrp.com>
2018-10-12 02:18:08 -0700

    fix

--- midi-app/assets/js/login.js
+++ midi-app/assets/js/login.js
@@ -1,17 +1,15 @@
-(function (cookieBakery) {
-    $('.login-submit-button').on('click', function (e) {
-        e.preventDefault();
-        var email = $('.email').val();
-        var pass = $('.pass').val();
-        
-        if (email === 'sales@midigator.com' && pass === 'sales123') {
-            cookieBakery.bake('demo=true', 1);
-            window.location.href = 'sales/30/loading.html'
-        } else if (email === 'prevention@midigator.com' && pass === 'prevention123') {
-            cookieBakery.bake('demo=true', 1);
-            window.location.href = 'prevention/30/loading.html'
-        } else {
-            $('.growler').slideDown();
-        }
-    });
-})(cookieBakery)
+$('.login-submit-button').on('click', function (e) {
+    e.preventDefault();
+    var email = $('.email').val();
+    var pass = $('.pass').val();
+    
+    if (email === 'sales@midigator.com' && pass === 'sales123') {
+        cookieBakery.bake('demo=true', 1);
+        window.location.href = 'sales/30/loading.html'
+    } else if (email === 'prevention@midigator.com' && pass === 'prevention123') {
+        cookieBakery.bake('demo=true', 1);
+        window.location.href = 'prevention/30/loading.html'
+    } else {
+        $('.growler').slideDown();
+    }
+});
